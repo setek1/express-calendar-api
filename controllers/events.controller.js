82c728bfcd1307e166a3b6e7ff8d85657b83f824
Sebastian Esquivel <set1.esq@gmail.com>
2025-01-07 19:16:47 -0300

    configuracion de fechas y horas , zona horaria

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -1,15 +1,16 @@
 import { eventsModel } from "../models/events.model.js"
 
+import moment from "moment-timezone"
+
 
 const addEvent=async(req, res)=>{
 
     const {class_type, start_time, tea_id, client_id} = req.body
     
-    const startDate=new Date(start_time)
-    const endDate= new Date(startDate.getTime() + (90 * 60000))
-    const endtimeF= endDate.toISOString()
+    const startDate= moment.tz(start_time,"America/Santiago").format('YYYY-MM-DD HH:mm:ss')
+    const endDate= moment.tz(start_time,"America/Santiago").add(90, 'minutes').format('YYYY-MM-DD HH:mm:ss');
 
-    const newEvent=await eventsModel.create({class_type, start_time,end_time:endtimeF ,tea_id, client_id})
+    const newEvent=await eventsModel.create({class_type, start_time:startDate,end_time:endDate ,tea_id, client_id})
     
     
     return res.status(200).json({ok:'okey',event:newEvent})
--- models/events.model.js
+++ models/events.model.js
@@ -6,7 +6,13 @@ const create =async({class_type, start_time,end_time, tea_id, client_id})=>{
         text:`
         insert into events (class_type, start_time, end_time, tea_id, client_id)
         values ($1, $2, $3, $4, $5)
-        returning class_type, start_time , end_time , tea_id, client_id
+        returning 
+        class_type, 
+        to_char(start_time, 'YYYY-MM-DD HH24:MI:SS') as start_time,
+        to_char(end_time, 'YYYY-MM-DD HH24:MI:SS') as end_time,
+        tea_id, 
+        client_id ,
+        eid
         `,
         values:[class_type, start_time,end_time ,tea_id, client_id]
     }
